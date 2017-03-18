f45890b0156e97d7e4eb7b75ce879fe72efffbc0
Kash Goudarzi <kashgoudarzi@gmail.com>
2017-03-18 12:42:47 -0700

    update user interface for login

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-
-
 console.log('this is a node test');
 
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-app.listen(3000, function() {
-	console.log('listening on 3000')
+var db;
+
+MongoClient.connect('mongodb://kashgoudarzi:Kha.goud1@ds135680.mlab.com:35680/wherestom', function (err, database) {
+	if (err) {
+		return console.log(err);
+	}
+	db = database;
+	app.listen(3000, function () {
+		console.log('listening on 3000 boyyy');
+	});
 });
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res) {
-	res.send('Hello World');
+app.get('/', function(req, res){
+	fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
+		res.send(text);
+	});
+});
+app.post('/quotes', function (req, res) {
+	console.log(req.body);
 
-	res.sendFile(__dirname + '/index.html');
-})
-
+	db.collection('quotes').save(req.body, function (err, result) {
+		if (err) return console.log(err);
+		console.log('saved to database');
+		res.redirect('/');
+	});
+});
