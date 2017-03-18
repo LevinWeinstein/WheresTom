45becf5ad3f187711a96c03451bae10acdbbb70f
Kash Goudarzi <kashgoudarzi@gmail.com>
2017-03-18 10:12:38 -0700

    npm installed es6 and oauth

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+
+
 console.log('this is a node test');
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+	console.log('listening on 3000')
+});
+
+app.get('/', function(req, res) {
+	res.send('Hello World');
+
+	res.sendFile(__dirname + '/index.html');
+})
+
