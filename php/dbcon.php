6e54587b224b0db2116fdd22cb8fb9c35d027282
Kash Goudarzi <kashgoudarzi@gmail.com>
2017-03-18 18:44:21 -0700

    removed phpmyadmin

--- php/dbcon.php
+++ php/dbcon.php
@@ -1,5 +1,18 @@
 <?php
 
-$db = mysqli_connect('wherestom:8889', 'root', 'root', 'wherestom');
+$db = mysqli_connect('localhost', 'root', 'password', 'wherestom');
+if (developmentEnvironment()){
+	echo 'hi';
+	$db = mysqli_connect('wherestom:8889', 'root', 'root', 'wherestom');
+}else{
+	echo 'no';
+}
+
+function developmentEnvironment() {
+	if (!in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', "::1"))) {
+		return false;
+	}
+	return true;
+}
 
 ?>
